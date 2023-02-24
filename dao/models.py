a1c55083afad2a284376e14092b2b296549f7ec3
kane.zhu <kane.zhu@moppomobi.com>
2023-02-24 18:40:36 +0800

    :card_file_box: 优化数据库字段，并增加查询基础工具

--- dao/models.py
+++ dao/models.py
@@ -3,12 +3,39 @@
 # @Software: PyCharm
 # @Description:
 
+from flask_login import UserMixin
+import datetime
+
 from dao import db
 
+"""
+如果你将模型类定义在单独的模块中，那么必须在调用db.create_all()之前导入相应的模块，
+以便让SQLAlchemy获取模型类被创建时生成的表信息
+创建类 需继承UserMixin, 有用户属性
 
-class User(db.Model):
-    __tablename__ = 'tbl_users'
+* is_authenticated: 是否被验证
+* is_active: 是否被激活
+* is_anonymous: 是否是匿名用户
+* get_id(): 获得用户id,并转换成unicode类型
+"""
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "tbl_users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(60))
+    password = db.Column(db.String(300))
+    is_mfa = db.Column(db.Boolean, unique=True)
+    otp_secret_key = db.Column(db.String(120),info="mfa信息")
+    is_disabled = db.Column(db.Boolean, info="用户状态,是否禁用")
+    create_time = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
+    update_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+
+    def to_dict(self):
+        model_dict = dict(self.__dict__)
+        del model_dict['_sa_instance_state']
+        return model_dict
+
+    db.to_dict = to_dict
 
---
+++ dao/ops_db_users.py
@@ -0,0 +1,29 @@
+# @Author  : kane.zhu
+# @Time    : 2023/2/24 16:58
+# @Software: PyCharm
+# @Description:
+
+from dao.models import User,db
+from log_settings import logger
+
+# 注册用户
+def db_ops_reg_user(reg_user_info_dict):
+    try:
+        db.session.execute(User.__table__.insert(),[reg_user_info_dict])
+        db.session.commit()
+    except Exception as e:
+        logger.error(e)
+        return False
+
+    return True
+
+
+def db_ops_get_user(email):
+    try:
+        get_user_passwd,get_user_secret_key = db.session.query(User.password,User.otp_secret_key).filter(User.email == email).first()
+
+    except Exception as e:
+        logger.error(e)
+        return False
+    return get_user_passwd,get_user_secret_key
+
