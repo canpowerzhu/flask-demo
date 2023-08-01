2b520120842c114eb0c2f0f773d09ee05b53ee10
kane.zhu <kane.zhu@moppomobi.com>
2023-08-01 22:22:36 +0800

    :construction: 项目模块参数配置数据库设置;修改时间字段名称 date变更time

--- dao/models.py
+++ dao/models.py
@@ -4,10 +4,11 @@
 # @Description:
 
 import datetime
+import ipaddress
 
 from flask_login import UserMixin
 from sqlalchemy import UniqueConstraint
-
+from sqlalchemy.orm import validates
 from dao import db
 
 """
@@ -74,8 +75,8 @@ class Domainlist(db.Model):
     locked = db.Column(db.Boolean, info="是否锁定")
     autorenew_enabled = db.Column(db.Boolean, info="是否自动续费")
     name_status = db.Column(db.String(50), info="域名是否有解析")
-    expire_date = db.Column(db.DateTime, default=datetime.datetime.now, info="过期时间")
-    create_date = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="创建时间")
+    expire_time = db.Column(db.DateTime, default=datetime.datetime.now, info="过期时间")
+    create_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="创建时间")
 
 
 class WifiInfo(db.Model):
@@ -90,8 +91,8 @@ class WifiInfo(db.Model):
     wifi_manage_pass = db.Column(db.String(200), info="wifi管理员密码")
     wifi_connect_pass = db.Column(db.String(200), info="wifi连接密码")
     create_by = db.Column(db.String(50), info="创建者", default="admin")
-    create_date = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
-    update_date = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+    create_time = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
+    update_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
 
 
 ## 工单模块
@@ -110,8 +111,8 @@ class WorkOrder(db.Model):
     transfer_by = db.Column(db.String(50), info="转派发起人", default="admin")
     is_aborted = db.Column(db.Boolean, info="是否终止", default=False)
     create_by = db.Column(db.String(50), info="创建者", default="admin")
-    create_date = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
-    update_date = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+    create_time = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
+    update_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
 
 
 # 工单附件详情表
@@ -122,8 +123,8 @@ class WorkOderAttachInfo(db.Model):
     attach_url = db.Column(db.String(50), info="附件地址")
     # 这里设置附件的访问链接有效期，工单完成后的30天失效 资源进入冷冻期 使用STS模式进行访问控制
     create_by = db.Column(db.String(50), info="创建者", default="admin")
-    create_date = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
-    update_date = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+    create_time = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
+    update_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
 
 
 
@@ -138,8 +139,8 @@ class WorkOrderCategory(db.Model):
     # 由之前的status和deleted合并为blocked字段，禁用至灰色。无删除后唯一索引问题
     blocked = db.Column(db.Boolean, info="工单分类开启选项", default=False)
     create_by = db.Column(db.String(50), info="创建者", default="admin")
-    create_date = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
-    update_date = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+    create_time = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
+    update_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
 
     __table_args__ = (
         # 主分类和二级分类联合唯一
@@ -164,8 +165,8 @@ class WorkOrderFlow(db.Model):
     status = db.Column(db.Boolean, info="工单流程状态标识", default=False)
     deleted = db.Column(db.Boolean, info="工单流程逻辑删除标识", default=False)
     create_by = db.Column(db.String(50), info="创建者", default="admin")
-    create_date = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
-    update_date = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+    create_time = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
+    update_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
 
 
 ### 系统管理模块
@@ -178,5 +179,105 @@ class SysConfigInfo(db.Model):
     config_value = db.Column(db.String(50), info="配置值", nullable=True)
     config_group = db.Column(db.String(10), info="分类", nullable=True)
     description = db.Column(db.String(50), info="备注", nullable=True)
-    create_date = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
-    update_date = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+    create_time = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
+    update_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+
+
+
+
+### 发布模块
+class ProjectInfo(db.Model):
+    __tablename__ = "tbl_project_info"
+    id = db.Column(db.Integer, primary_key=True)
+    project_name = db.Column(db.String(50), info="项目名称")
+    project_code = db.Column(db.String(10), info="项目名称代码")
+    base_image_name = db.Column(db.String(50), info="/base/apline-base-arthas-jdk8:3.1.2")
+    health_check_interval = db.Column(db.Integer, info="两次健康检查间隔，默认30s",defautl=30)
+    health_check_timeout = db.Column(db.Integer, info="健康检查超过这个时间，则失败，默认30s",defautl=30)
+    health_check_retries = db.Column(db.Integer, info="连续检查失败次数超过，则失败，默认3",defautl=3)
+    health_check_start_period = db.Column(db.Integer, info="应用初始化时间，启动过程 健康检查不计入，默认30s",defautl=30)
+    project_ico= db.Column(db.String(10), info="项目icon地址，来自oss地址")
+    description = db.Column(db.String(50), info="备注", nullable=True)
+    create_time = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
+    update_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+
+class ModuleInfo(db.Model):
+    __tablename__ = "tbl_module_info"
+    id = db.Column(db.Integer, primary_key=True)
+    module_name = db.Column(db.String(50), info="模块名称")
+    module_package_name = db.Column(db.String(50), info="模块包名称")
+    module_rel_path = db.Column(db.String(50), info="模块包的相对路径")
+    module_env_pairs = db.Column(db.Text) #环境变量---json字符串{"TZ":"Asia/Shanghai","GOOGLE_API_KEY":"123456key"}
+    module_port_pairs = db.Column(db.Text) #端口协议---json字符串{5000:"tcp",5001:"udp",5002:"tcp"}
+    module_host_pairs = db.Column(db.Text) #主机解析---json字符串{"moppo-xxl": "192.168.9.227","scrm-bus-es":"192.168.3.4"}
+    ## 目录挂载解析---json字符串
+    # {"/data/logs/{module_name}/logs": "/mnt/logs",
+    # "/data/app/{module_name}/{module_package_name}":"/mnt/scrm-bus-server.jar"
+    # }
+    module_volumes_pairs = db.Column(db.Text)
+    # 当dump_oom_status为True, 启动参数增加 -XX:+HeapDumpOnOutOfMemoryError
+    dump_oom_status = db.Column(db.Boolean, info="发生OOM时，是否dump", default=False)
+    # 设置后 启动参数增加  -XX:HeapDumpPath=/tmp/dump.hprof
+    dump_oom_path = db.Column(db.String(50), info="dump文件存储位置", default='/tmp/dump.hprof')
+
+    # debug_status, 启动参数增加 -agentlib:jdwp=transport=dt_socket,address={{ debug_status_port }},server=y,suspend=n
+    debug_status = db.Column(db.Boolean, info="是否开启debug启动模式", default=False)
+    # debug开启后，debug_status_port 这个值不能为空
+    debug_status_port = db.Column(db.String(5), info="开启debug时的端口")
+    # 启动的自定义参数 存储json类型
+    #{"file.encoding":"UTF-8","spring.profiles.active":"prod"}  拼接成"-Dfile.encoding=UTF-8”
+    start_define_params = db.Column(db.Text)
+    module_memory = db.Column(db.Integer, info="模块启动时的堆栈内存，Xmx Xms")
+
+
+
+    project_id = db.Column(db.Integer, info="模块归属项目ID")
+    #
+    project_cloud_platform = db.Column(db.Integer, info="部署的平云台，意味着走哪个代理")
+    description = db.Column(db.String(50), info="备注", nullable=True)
+    create_time = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
+    update_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+
+    # 当开启dump后，必须输入dump文件的存储路径
+    @validates('dump_oom_status')
+    def validates_dump_oom_path(self,key,dump_oom_status):
+        if dump_oom_status and not self.dump_oom_path:
+            raise  ValueError("validates_dump_oom_path field cannot be empty when dump_oom_status is True.")
+
+        return dump_oom_status
+
+
+    # 当开启debug后，debug_status_port不能为空
+    @validates('debug_status')
+    def validates_debug_status_port(self, key, debug_status):
+        if debug_status and not self.debug_status_port:
+            raise ValueError("debug_status_port field cannot be empty when debug_status is True.")
+
+        return debug_status
+
+
+class CloudPlatformInfo(db.Model):
+    __tablename__ = "tbl_cloud_platform_info"
+    id = db.Column(db.Integer, primary_key=True)
+    cloud_platform_name = db.Column(db.String(20), info="公云平台名称")
+    cloud_platform_code = db.Column(db.String(20), info="公云平台名称代码 AliCloud、HuaWeiCloud、AwsCloud")
+
+    cloud_platform_proxy_active = db.Column(db.Boolean, info="云平台代理是否激活", default=False)
+    cloud_platform_proxy_host = db.Column(db.String(10), info="代理主机地址")
+    cloud_platform_proxy_port = db.Column(db.String(5), info="代理主机端口")
+    cloud_platform_proxy_username = db.Column(db.String(10), info="代理主机登陆用户名")
+    # 密码和密钥可以同时存在
+    cloud_platform_proxy_password = db.Column(db.String(10), info="代理主机SSH密码")
+    cloud_platform_proxy_ksa_key = db.Column(db.String(1000), info="代理主机SSH密钥")
+    description = db.Column(db.String(50), info="备注", nullable=True)
+    create_time = db.Column(db.DateTime, default=datetime.datetime.now, info="创建时间")
+    update_time = db.Column(db.DateTime, onupdate=datetime.datetime.now, info="更新时间")
+
+    def validate_cloud_platform_proxy_host(self, key, cloud_platform_proxy_host):
+        try:
+            # Try to create an IP address object from the input
+           ipaddress.ip_address(cloud_platform_proxy_host)
+        except ValueError:
+            # If the input is not a valid IP address, raise a ValueError
+            raise ValueError(f"{key} field must be a valid IPv4 or IPv6 address.")
+        return cloud_platform_proxy_host
