31bd04fdc3e9a76419ca1a8a52bae35264a09ecf
kane.zhu <kane.zhu@moppomobi.com>
2023-08-16 21:00:25 +0800

    :mag: 增加汉字转全拼的工具类

---
+++ utils/transfer_to_pinyin.py
@@ -0,0 +1,25 @@
+# @Author  : kane.zhu
+# @Time    : 2023/8/15 21:45
+# @Software: PyCharm
+# @Description:
+
+from xpinyin import Pinyin
+
+
+#初始化拼音对象
+
+def words_transfer_to_letter(words: str)-> str:
+    """
+    将汉字转换为以_拼接的全拼大写字母
+    例如：你好-> NI_HAO
+    :param words:
+    :return:
+    """
+    P = Pinyin()
+    r = P.get_pinyin(words)
+    result = '_'.join(i.upper() for i in r.split('-'))
+    return result
+
+# print(words_transfer_to_letter("销售宝"))
+
+
